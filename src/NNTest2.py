937f71c3f6a761d48b3544ee0c05ab405206b8e5
John Halazonetis <johnhalazonetis@gmail.com>
2020-01-11 11:06:16 +0100

    Training data now works! Will move onto working with the GPU

--- src/NNTest2.py
+++ src/NNTest2.py
@@ -7,15 +7,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-REBUILD_DATA = False
 
-class DogsVsCats():
+REBUILD_DATA = True    # set to true to one once, then back to false unless you want to change something in your training data.
+
+
+class DogsVSCats:
     IMG_SIZE = 50
     CATS = "../PetImages/Cat"
     DOGS = "../PetImages/Dog"
+    TESTING = "../PetImages/Testing"
     LABELS = {CATS: 0, DOGS: 1}
-
     training_data = []
+
     catcount = 0
     dogcount = 0
 
@@ -23,42 +26,37 @@ class DogsVsCats():
         for label in self.LABELS:
             print(label)
             for f in tqdm(os.listdir(label)):
-                try:
-                    path = os.path.join(label, f)
-                    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-                    img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
-                    self.training_data.append([np.array(img), np.eye(10)[self.LABELS[label]]])
+                if "jpg" in f:
+                    try:
+                        path = os.path.join(label, f)
+                        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+                        img = cv2.resize(img, (self.IMG_SIZE, self.IMG_SIZE))
+                        self.training_data.append([np.array(img), np.eye(2)[self.LABELS[label]]])  # do something like print(np.eye(2)[1]), just makes one_hot
+                        # print(np.eye(2)[self.LABELS[label]])
 
-                    if label == self.CATS:
-                        self.catcount += 1
-                    elif label == self.DOGS:
-                        self.dogcount += 1
+                        if label == self.CATS:
+                            self.catcount += 1
+                        elif label == self.DOGS:
+                            self.dogcount += 1
 
-                except Exception as e:
-                    pass
-                    #print(str(e))
+                    except Exception as e:
+                        pass
+                        # print(label, f, str(e))
 
         np.random.shuffle(self.training_data)
         np.save("training_data.npy", self.training_data)
-        print("Cats: ", self.catcount)
-        print("Dogs: ", self.dogcount)
+        print('Cats:', dogsvcats.catcount)
+        print('Dogs:', dogsvcats.dogcount)
 
-if REBUILD_DATA:
-    dogsvcats = DogsVsCats()
-    dogsvcats.make_training_data()
-
-training_data = np.load("training_data.npy", allow_pickle=True)
-
-# Now we build the model itself
 
 class Net(nn.Module):
     def __init__(self):
-        super().__init__() # just run the init of parent class (nn.Module)
-        self.conv1 = nn.Conv2d(1, 32, 5)    # Input is 1 image, 32 output channels, 5x5 kernel / window
-        self.conv2 = nn.Conv2d(32, 64, 5)   # Input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 kernel / window
+        super().__init__()  # just run the init of parent class (nn.Module)
+        self.conv1 = nn.Conv2d(1, 32, 5)    # input is 1 image, 32 output channels, 5x5 kernel / window
+        self.conv2 = nn.Conv2d(32, 64, 5)   # input is 32, bc the first layer output 32. Then we say the output will be 64 channels, 5x5 kernel / window
         self.conv3 = nn.Conv2d(64, 128, 5)
 
-        x = torch.randn(50,50).view(-1,1,50,50)
+        x = torch.randn(50, 50).view(-1, 1, 50, 50)
         self._to_linear = None
         self.convs(x)
 
@@ -79,11 +77,19 @@ class Net(nn.Module):
         x = self.convs(x)
         x = x.view(-1, self._to_linear)  # .view is reshape ... this flattens X before
         x = F.relu(self.fc1(x))
-        x = self.fc2(x) # bc this is our output layer. No activation here.
+        x = self.fc2(x)     # bc this is our output layer. No activation here.
         return F.softmax(x, dim=1)
+
 
 net = Net()
 print(net)
+
+if REBUILD_DATA:
+    dogsvcats = DogsVSCats()
+    dogsvcats.make_training_data()
+
+training_data = np.load("training_data.npy", allow_pickle=True)
+print(len(training_data))
 
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 loss_function = nn.MSELoss()
@@ -92,10 +98,8 @@ X = torch.Tensor([i[0] for i in training_data]).view(-1, 50, 50)
 X = X/255.0
 y = torch.Tensor([i[1] for i in training_data])
 
-# Define our testing dataset:
 VAL_PCT = 0.1  # lets reserve 10% of our data for validation
 val_size = int(len(X)*VAL_PCT)
-print(val_size)
 
 train_X = X[:-val_size]
 train_y = y[:-val_size]
@@ -103,39 +107,44 @@ train_y = y[:-val_size]
 test_X = X[-val_size:]
 test_y = y[-val_size:]
 
-print(len(train_X), len(test_X))
-
-# Now we figure our what we actually want to train:
 BATCH_SIZE = 100
 EPOCHS = 1
 
-for epoch in range(EPOCHS):
-    for i in tqdm(range(0, len(train_X), BATCH_SIZE)):  # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
-        # print(f"{i}:{i+BATCH_SIZE}")
-        batch_X = train_X[i:i+BATCH_SIZE].view(-1, 1, 50, 50)
-        batch_y = train_y[i:i+BATCH_SIZE]
 
-        net.zero_grad()
+def train(net):
+    for epoch in range(EPOCHS):
+        for i in tqdm(range(0, len(train_X), BATCH_SIZE)):  # from 0, to the len of x, stepping BATCH_SIZE at a time. [:50] ..for now just to dev
+            # print(f"{i}:{i+BATCH_SIZE}")
+            batch_X = train_X[i:i+BATCH_SIZE].view(-1, 1, 50, 50)
+            batch_y = train_y[i:i+BATCH_SIZE]
 
-        outputs = net(batch_X)
-        loss = loss_function(outputs, batch_y)
-        loss.backward()
-        optimizer.step()    # Does the update
+            net.zero_grad()
 
-    print(f"Epoch: {epoch}. Loss: {loss}")
+            outputs = net(batch_X)
+            loss = loss_function(outputs, batch_y)
+            loss.backward()
+            optimizer.step()    # Does the update
 
-# We calculate the accuracy of the NN
-"""
-correct = 0
-total = 0
+        print(f"Epoch: {epoch}. Loss: {loss}")
 
-with torch.no_grad():
-    for i in tqdm(range(len(test_X))):
-        real_class = torch.argmax(test_y[i])
-        net_out = net(test_X[i].view(-1, 1, 50, 50))[0]
-        predicted_class = torch.argmax(net_out)
-        if predicted_class == real_class:
-            correct += 1
-        total += 1
-print("Accuracy: ", round(correct/total, 3))
-"""
+
+def test(net):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for i in tqdm(range(len(test_X))):
+            real_class = torch.argmax(test_y[i])
+            net_out = net(test_X[i].view(-1, 1, 50, 50))[0]     # returns a list,
+            predicted_class = torch.argmax(net_out)
+
+            if predicted_class == real_class:
+                correct += 1
+            total += 1
+
+    print("Accuracy: ", round(correct/total, 3))
+
+
+train(net)
+
+test(net)
+
