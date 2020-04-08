253ce3ade3ac6b2fb317c24a00b27c4cc54ac641
John Halazonetis <john.halazonetis@icloud.com>
2020-04-08 02:06:03 +0200

    Added camera calibration capabilities to aruco script

--- src/aruco.cpp
+++ src/aruco.cpp
@@ -5,6 +5,8 @@
 #include <opencv2/aruco.hpp>
 #include <opencv2/calib3d.hpp>
 
+#include <time.h>
+
 #include <sstream>
 #include <iostream>
 #include <fstream>
@@ -12,19 +14,19 @@
 using namespace std;
 using namespace cv;
 
-const float calibrationSquareDimension = 0.03f;   // Dimension of side of one square [m]
+const float calibrationSquareDimension = 0.03f;     // Dimension of side of one square [m]
 const float arucoSquareDimenstion = 0.0382f;        // Dimension of side of one aruco square [m]
-const Size chessboardDimensions = Size(7, 7);       // Number of square on Chessboard calibration page
+const Size chessboardDimensions = Size(4, 8);       // Number of square on Chessboard calibration page
 
-void createArucoMarkers()       // Function to create the Aruco markers for us and put them in the "markers" directory
+void createArucoMarkers()                           // Function to create the Aruco markers for us and put them in the "markers" directory
 {
-    Mat outputMarker;           // Define matrix where we have the output marker
+    Mat outputMarker;                               // Define matrix where we have the output marker
 
     Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);       // Define the marker dictionary from the standard dictionary, we take the dictionary with 50 markers of 4x4 squares
 
-    for (int i = 0; i < 50; i++)        // For loop to make each marker
+    for (int i = 0; i < 50; i++)                    // For loop to make each marker
     {
-        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);       // Load aruco marker 
+        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);       // Load aruco marker
 
         ostringstream convert;
 
@@ -34,10 +36,10 @@ void createArucoMarkers()       // Function to create the Aruco markers for us a
 
         imwrite(convert.str(), outputMarker);           // Write and save the file to the desired location
     }
-    
+
 }
 
-void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f>& corners)     // Function to
+void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f>& corners)
 {
     for (int i = 0; i < boardSize.height; i++)
     {
@@ -70,6 +72,60 @@ void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundC
     }
 }
 
+void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distanceCoefficients)
+{
+    vector<vector<Point2f>> checkerboardImageSpacePoints;
+    getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+
+    vector<vector<Point3f>> worldSpaceCornerPoints(1);
+
+    createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
+    worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+
+    vector<Mat> rVectors, tVectors;
+    distanceCoefficients = Mat::zeros(8, 1, CV_64F);
+
+    calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
+
+
+}
+
+bool saveCameraCalibration(string name, Mat cameraMatrix, Mat distanceCoefficients)
+{
+    ofstream outStream(name);
+    if (outStream)
+    {
+        uint16_t rows = cameraMatrix.rows;
+        uint16_t columns = cameraMatrix.cols;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                double value = cameraMatrix.at<double>(r, c);
+                outStream << value << endl;
+            }
+        }
+
+        rows = distanceCoefficients.rows;
+        columns = distanceCoefficients.cols;
+
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < columns; c++)
+            {
+                double value = distanceCoefficients.at<double>(r, c);
+                outStream << value << endl;
+            }
+        }
+
+        outStream.close();
+        return true;
+    }
+
+    return false;
+}
+
 int main(int argv, char** argc)
 {
     Mat frame;                                                      // Define frame as matrix
@@ -78,9 +134,11 @@ int main(int argv, char** argc)
 
     Mat distanceCoefficients;                                       // Define distance coefficients matrix
 
+    vector<Mat> savedImages;
+
     vector<vector<Point2f>> markerCorners, rejectedCandidates;
 
-    VideoCapture vid("../videos/chessboard-2.mov");                 // Define VideoCapture element (where we define the input source of the video)
+    VideoCapture vid("../videos/chessboard-4.mov");                 // Define VideoCapture element (where we define the input source of the video)
 
     if (!vid.isOpened())                                            // If no video is detected, exit the program
     {
@@ -91,8 +149,13 @@ int main(int argv, char** argc)
 
     namedWindow("Video Input", WINDOW_AUTOSIZE);                    // Make window with video input
 
+    long double delay = 1000 / framesPerSecond;
+
+    long double duration;
+
     while(true)                                                     // Loop until exit
     {
+
         if (!vid.read(frame))                                       // If we can't read the frame, break the loop
         {
             break;
@@ -101,17 +164,43 @@ int main(int argv, char** argc)
         vector<Vec2f> foundPoints;                                  // Define vector where the found points are on the image
 
 
-        bool found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_FAST_CHECK);  // Use findChessboardCorners function (built into OpenCV) to find points on checker board and put the coordinates into foundPoint vector
+        bool found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_NORMALIZE_IMAGE);     // Use findChessboardCorners function (built into OpenCV) to find points on checker board and put the coordinates into foundPoint vector
 
-        drawChessboardCorners(frame, chessboardDimensions, foundPoints, found);                             // Use drawChessboardCorners function (built into OpenCV) to draw the chessboard corners on the drawToFrame frame
+        drawChessboardCorners(frame, chessboardDimensions, foundPoints, found);                                     // Use drawChessboardCorners function (built into OpenCV) to draw the chessboard corners on the drawToFrame frame
 
-        for (auto i = foundPoints.begin(); i != foundPoints.end(); ++i)
-            {cout << *i << endl;}
-        cout << endl;
 
-        imshow("Video", frame);
+        imshow("Video", frame);                                                                                     // Show the frame
 
-        waitKey(1);
+        char character = waitKey(1);
+
+        switch(character)
+        {
+            case ' ':
+                // saving image
+                cout << "Input <SPACE> recieved" << endl;
+                if (found)
+                {
+                    Mat temp;
+                    frame.copyTo(temp);
+                    savedImages.push_back(temp);
+                }
+                break;
+            case 13:
+                // start calibration
+                cout << "Input <ENTER> recieved" << endl;
+                if (savedImages.size() > 15)
+                {
+                    cameraCalibration(savedImages, chessboardDimensions, calibrationSquareDimension, cameraMatrix, distanceCoefficients);
+                    saveCameraCalibration("cameraCalibration", cameraMatrix, distanceCoefficients);
+                }
+
+                break;
+            case 27:
+                // exit
+                cout << "Input <ESC> recieved" << endl;
+                return 0;
+                break;
+        }
 
     }
 
