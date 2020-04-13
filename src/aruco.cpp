109c7c630242aa87b297cba7b28a7a3faf2566c7
John Halazonetis <john.halazonetis@icloud.com>
2020-04-13 11:21:00 +0200

    Small updates to aruco code

--- src/aruco.cpp
+++ src/aruco.cpp
@@ -16,23 +16,23 @@ const float calibrationSquareDimension = 0.03f;     // Dimension of side of one 
 const float arucoSquareDimension = 0.0382f;         // Dimension of side of one aruco square [m]
 const Size chessboardDimensions = Size(4, 8);       // Number of square on Chessboard calibration page
 
-void createArucoMarkers()                           // Function to create the Aruco markers for us and put them in the "markers" directory
+void createArucoMarkers()                                               // Function to create the Aruco markers for us and put them in the "markers" directory
 {
-    Mat outputMarker;                               // Define matrix where we have the output marker
+    Mat outputMarker;                                                   // Define matrix where we have the output marker
 
     Ptr<aruco::Dictionary> markerDictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME::DICT_4X4_50);       // Define the marker dictionary from the standard dictionary, we take the dictionary with 50 markers of 4x4 squares
 
-    for (int i = 0; i < 50; i++)                    // For loop to make each marker
+    for (int i = 0; i < 50; i++)                                        // For loop to make each marker
     {
-        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);       // Load aruco marker
+        aruco::drawMarker(markerDictionary, i, 500, outputMarker, 1);   // Load aruco marker
 
         ostringstream convert;
 
-        string imageName = "../markers/4x4Marker_";     // Directory where we put the marker file
+        string imageName = "../markers/4x4Marker_";                     // Directory where we put the marker file
 
-        convert << imageName << i << ".jpg";            // Define name and type of image we output
+        convert << imageName << i << ".jpg";                            // Define name and type of image we output
 
-        imwrite(convert.str(), outputMarker);           // Write and save the file to the desired location
+        imwrite(convert.str(), outputMarker);                           // Write and save the file to the desired location
     }
 
 }
@@ -114,18 +114,18 @@ int startCameraMonitoring(const Mat& cameraMatrix, const Mat& distanceCoefficien
 
 void cameraCalibration(vector<Mat> calibrationImages, Size boardSize, float squareEdgeLength, Mat& cameraMatrix, Mat& distanceCoefficients)
 {
-    vector<vector<Point2f>> checkerboardImageSpacePoints;
-    getChessboardCorners(calibrationImages, checkerboardImageSpacePoints, false);
+    vector<vector<Point2f>> chessboardImageSpacePoints;
+    getChessboardCorners(calibrationImages, chessboardImageSpacePoints, false);
 
     vector<vector<Point3f>> worldSpaceCornerPoints(1);
 
     createKnownBoardPosition(boardSize, squareEdgeLength, worldSpaceCornerPoints[0]);
-    worldSpaceCornerPoints.resize(checkerboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
+    worldSpaceCornerPoints.resize(chessboardImageSpacePoints.size(), worldSpaceCornerPoints[0]);
 
     vector<Mat> rVectors, tVectors;
     distanceCoefficients = Mat::zeros(8, 1, CV_64F);
 
-    calibrateCamera(worldSpaceCornerPoints, checkerboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
+    calibrateCamera(worldSpaceCornerPoints, chessboardImageSpacePoints, boardSize, cameraMatrix, distanceCoefficients, rVectors, tVectors);
 
 
 }
@@ -224,7 +224,7 @@ int main(int argv, char** argc)
 {
     Mat frame;                                                      // Define frame as matrix
 
-    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);           // Define camera calibration matrix
+    Mat cameraMatrix = Mat::eye(3, 3, CV_64F);                      // Define camera calibration matrix
 
     Mat distanceCoefficients;                                       // Define distance coefficients matrix
 
@@ -254,9 +254,9 @@ int main(int argv, char** argc)
         vector<Vec2f> foundPoints;                                  // Define vector where the found points are on the image
 
 
-        bool found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_NORMALIZE_IMAGE);     // Use findChessboardCorners function (built into OpenCV) to find points on checker board and put the coordinates into foundPoint vector
+        bool found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_NORMALIZE_IMAGE);     // Use findChessboardCorners function (built into OpenCV) to find points on chessboard and put coordinates into foundPoint vector
 
-        drawChessboardCorners(frame, chessboardDimensions, foundPoints, found);                                     // Use drawChessboardCorners function (built into OpenCV) to draw the chessboard corners on the drawToFrame frame
+        drawChessboardCorners(frame, chessboardDimensions, foundPoints, found);                                     // Use drawChessboardCorners function (built into OpenCV) to draw chessboard corners on drawToFrame frame
 
 
         imshow("Video", frame);                                                                                     // Show the frame
